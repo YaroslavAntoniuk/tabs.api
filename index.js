0bf1f62bc4d8eade70dde10ae311fa00348192c1
YaroslavAntoniuk <yaroslav.antoniuk@binary-studio.com>
2022-05-01 15:46:08 +0300

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const { readFile } = require('fs/promises');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get('/', async (req, res, next) => {
   try {
